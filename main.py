5f1a1d722e991dcad160cbac54c05d11fd923b77
jorijnsmit <mail@gosuto.ai>
2022-07-01 14:59:54 +0200

    update fantom api url

--- main.py
+++ main.py
@@ -15,16 +15,16 @@ SAFES = {
     '0xb364bAb258ad35dd83c7dd4E8AC78676b7aa1e9F': ['dev', 'Arbitrum', 3],
     '0x329543f0F4BB134A3f7a826DC32532398B38a3fA': ['dev', 'Binance Smart Chain', 2],
     '0x4977110Ed3CD5eC5598e88c8965951a47dd4e738': ['dev', 'Polygon', 3],
-    # '0x4c56ee3295042f8A5dfC83e770a21c707CB46f5b': ['dev', 'Fantom', 3],
+    '0x4c56ee3295042f8A5dfC83e770a21c707CB46f5b': ['dev', 'Fantom', 3],
     '0x86cbD0ce0c087b482782c181dA8d191De18C8275': ['techops', 'Mainnet', 3],
     '0x292549E6bd5a41aE4521Bb8679aDA59631B9eD4C': ['techops', 'Arbitrum', 3],
     # '0x777061674751834993bfBa2269A1F4de5B4a6E7c': ['techops', 'Binance Smart Chain', 3],
     '0xeb7341c89ba46CC7945f75Bd5dD7a04f8FA16327': ['techops', 'Polygon', 3],
-    # '0x781E82D5D49042baB750efac91858cB65C6b0582': ['techops', 'Fantom', 3],
+    '0x781E82D5D49042baB750efac91858cB65C6b0582': ['techops', 'Fantom', 3],
     '0xD0A7A8B98957b9CD3cFB9c0425AbE44551158e9e': ['treasury_vault', 'Mainnet', 5],
     '0x45b798384c236ef0d78311D98AcAEc222f8c6F54': ['treasury_vault', 'Fantom', 3],
     '0x042B32Ac6b453485e357938bdC38e0340d4b9276': ['treasury_ops', 'Mainnet', 3],
-    # '0xf109c50684EFa12d4dfBF501eD4858F25A4300B3': ['treasury_ops', 'Fantom', 3],
+    '0xf109c50684EFa12d4dfBF501eD4858F25A4300B3': ['treasury_ops', 'Fantom', 3],
     '0xA9ed98B5Fb8428d68664f3C5027c62A10d45826b': ['treasury_voter', 'Mainnet', 5],
     '0xD4868d98849a58F743787c77738D808376210292': ['fin_ops', 'Mainnet', 3],
     '0x6F76C6A1059093E21D8B1C13C4e20D8335e2909F': ['politician', 'Mainnet', 3],
@@ -36,7 +36,7 @@ GNOSIS_URLS  = {
     'Binance Smart Chain': 'https://safe-transaction.bsc.gnosis.io',
     'Polygon': 'https://safe-transaction.polygon.gnosis.io',
     'Arbitrum': 'https://safe-transaction.arbitrum.gnosis.io',
-    'Fantom': 'https://safe.fantom.network'
+    'Fantom': 'https://safe-txservice.fantom.network'
 }
 GNOSIS_SLUGS = {
     'Mainnet': 'eth',
