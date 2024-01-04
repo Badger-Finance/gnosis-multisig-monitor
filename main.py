46cf11de5b3ba0be001057e226360f10612f7d3a
mrbasado <mrbasado@protonmail.com>
2024-01-04 10:39:11 -0600

    feat: remove fantom

--- main.py
+++ main.py
@@ -22,17 +22,17 @@ SAFES = {
     "0xb364bAb258ad35dd83c7dd4E8AC78676b7aa1e9F": ["dev", "Arbitrum", 3],
     "0x329543f0F4BB134A3f7a826DC32532398B38a3fA": ["dev", "Binance Smart Chain", 2],
     "0x4977110Ed3CD5eC5598e88c8965951a47dd4e738": ["dev", "Polygon", 3],
-    "0x4c56ee3295042f8A5dfC83e770a21c707CB46f5b": ["dev", "Fantom", 3],
+    # "0x4c56ee3295042f8A5dfC83e770a21c707CB46f5b": ["dev", "Fantom", 3],
     "0x0D5eDB3ECbB15EF4EaD105c018fEd4e1d173B335": ["dev", "Optimism", 3],
     "0x86cbD0ce0c087b482782c181dA8d191De18C8275": ["techops", "Mainnet", 3],
     "0x292549E6bd5a41aE4521Bb8679aDA59631B9eD4C": ["techops", "Arbitrum", 3],
     "0xeb7341c89ba46CC7945f75Bd5dD7a04f8FA16327": ["techops", "Polygon", 3],
-    "0x781E82D5D49042baB750efac91858cB65C6b0582": ["techops", "Fantom", 3],
+    # "0x781E82D5D49042baB750efac91858cB65C6b0582": ["techops", "Fantom", 3],
     "0x8D05c5DA2a3Cb4BeB4C5EB500EE9e3Aa71670733": ["techops", "Optimism", 2],
     "0xD0A7A8B98957b9CD3cFB9c0425AbE44551158e9e": ["treasury_vault", "Mainnet", 5],
-    "0x45b798384c236ef0d78311D98AcAEc222f8c6F54": ["treasury_vault", "Fantom", 3],
+    # "0x45b798384c236ef0d78311D98AcAEc222f8c6F54": ["treasury_vault", "Fantom", 3],
     "0x042B32Ac6b453485e357938bdC38e0340d4b9276": ["treasury_ops", "Mainnet", 3],
-    "0xf109c50684EFa12d4dfBF501eD4858F25A4300B3": ["treasury_ops", "Fantom", 3],
+    # "0xf109c50684EFa12d4dfBF501eD4858F25A4300B3": ["treasury_ops", "Fantom", 3],
     "0xA9ed98B5Fb8428d68664f3C5027c62A10d45826b": ["treasury_voter", "Mainnet", 5],
     "0xB76782B51BFf9C27bA69C77027e20Abd92Bcf3a8": ["ibbtc", "Mainnet", 3],
 }
@@ -42,7 +42,7 @@ GNOSIS_URLS = {
     "Polygon": "https://safe-transaction-polygon.safe.global/",
     "Arbitrum": "https://safe-transaction-arbitrum.safe.global/",
     "Optimism": "https://safe-transaction-optimism.safe.global/",
-    "Fantom": "https://safe-txservice.fantom.network",
+    # "Fantom": "https://safe-txservice.fantom.network",
 }
 GNOSIS_SLUGS = {
     "Mainnet": "eth",
@@ -56,7 +56,7 @@ EXPLORER_URLS = {
     "Binance Smart Chain": "https://bscscan.com/tx/",
     "Polygon": "https://polygonscan.com/tx/",
     "Arbitrum": "https://arbiscan.io/tx/",
-    "Fantom": "https://ftmscan.com/tx/",
+    # "Fantom": "https://ftmscan.com/tx/",
     "Optimism": "https://optimistic.etherscan.io/tx/",
 }
 MENTIONS = {
